c22caab7cce0558d7665079205772fb42b3e103d
PoorMansTurbo <nickdutka@gmail.com>
2023-01-19 14:14:57 -0800

    Added Input text exercise.

--- Assets/GuessingGame/GuessingGame.cs
+++ Assets/GuessingGame/GuessingGame.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GuessingGame : MonoBehaviour
 {
+    public TextMeshProUGUI textGameObject;
+    public TMP_InputField inputFieldGameObject;
     public void MyFunction()
     {
+        textGameObject.text = "Hello " + inputFieldGameObject.text + " you are " + Random.Range(1, 100) + " years old.";
+        
+        
         Debug.Log("HELLO");
     }
+
+    
+
 
     // Start is called before the first frame update
     void Start()
